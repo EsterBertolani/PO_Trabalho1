910a793775c29d45dfc4bc69abd8703a29b8edd3
Vanderson <127350791+Vandin-alvsr@users.noreply.github.com>
2025-09-09 18:13:09 -0300

    Add files via upload

---
+++ AlgoritmosOrdenacao/Resultado.java
@@ -0,0 +1,14 @@
+package AlgoritmosOrdenacao;
+
+public class Resultado {
+    public long comparacoes;
+    public long movimentacoes;
+    public long tempo;
+
+    public Resultado (long comp, long mov, long temp) {
+        this.comparacoes = comp;
+        this.movimentacoes = mov;
+        this.tempo = temp;
+
+    }
+}
